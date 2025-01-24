95b0cecfc3bce251456e34b0610d981ae806f4e0
msyed2020 <mikailsyed@yahoo.com>
2025-01-24 14:26:38 -0500

    Two Sum done

---
+++ twosum.py
@@ -0,0 +1,13 @@
+def twoSum(arr, target):
+    h = {}
+    for i, j in enumerate(arr):
+        val = target - j
+        if val in h:
+            return [h[val], i]
+        else:
+            h[j] = i
+    return
+
+print(twoSum([2,7,11,15], 9))
+print(twoSum([3,2,4], 6))
+print(twoSum([3,3], 6))
