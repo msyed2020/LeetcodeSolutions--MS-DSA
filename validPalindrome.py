5c8bfe8a677bac9eb64b9d46f9bf33bffc5be746
msyed2020 <mikailsyed@yahoo.com>
2025-01-25 12:40:47 -0500

    Second/final solution

--- validPalindrome.py
+++ validPalindrome.py
@@ -1,4 +1,17 @@
 def validPalindrome(word: str):
     return word == word[::-1]
 
+def validPalindrome2(word: str):
+    word = ''.join(c.lower() for c in word if c.isalnum())
+    l = 0
+    r = len(word) - 1
+    while l <= r:
+        if word[l] != word[r]:
+            return False
+        else:
+            l += 1
+            r -= 1
+    return True
+
 print(validPalindrome("nitin"))
+print(validPalindrome2("nitin"))
