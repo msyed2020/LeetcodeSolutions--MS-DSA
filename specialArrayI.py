d669e38b5e6c9494070eeab38374c3d4efe825cd
msyed2020 <mikailsyed@yahoo.com>
2025-01-31 20:10:09 -0500

    Add special array I solution

---
+++ specialArrayI.py
@@ -0,0 +1,11 @@
+def isArraySpecial(nums: list[int]) -> bool:
+    if len(nums) == 1:
+        return True
+    for i in range(len(nums)-1):
+        if (nums[i] % 2 == 0 and nums[i + 1] % 2 == 0) or (nums[i] % 2 == 1 and nums[i + 1] % 2 == 1):
+            return False
+    return True
+
+print(isArraySpecial([1]))
+print(isArraySpecial([2,1,4]))
+print(isArraySpecial([4,3,1,6]))
