774a60db8c8e3ea28aa3824ac814cd5401386056
msyed2020 <mikailsyed@yahoo.com>
2025-02-01 18:31:39 -0500

    Add sol

--- bestTimeToBuySellStock.py
+++ bestTimeToBuySellStock.py
@@ -1,1 +1,12 @@
-# To be completed
+def maxProfit(self, prices: list[int]) -> int:
+        buy = 0
+        sell = 1
+        maxProfit = 0
+        for _ in range(len(prices)-1):
+            if prices[buy] < prices[sell]:
+                profit = prices[sell] - prices[buy]
+                maxProfit = max(maxProfit, profit)
+            else:
+                buy = sell
+            sell += 1
+        return maxProfit
