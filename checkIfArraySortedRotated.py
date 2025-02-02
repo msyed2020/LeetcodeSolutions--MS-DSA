3ead73259152fee40cac3b0014d39a8bcb0affe9
msyed2020 <mikailsyed@yahoo.com>
2025-02-02 13:32:51 -0500

    Add more

--- checkIfArraySortedRotated.py
+++ checkIfArraySortedRotated.py
@@ -1,7 +1,11 @@
-def check(self, nums: List[int]) -> bool:
+def check(self, nums: list[int]) -> bool:
         count, n = 0, len(nums)
         for i in range(n):
             if nums[i] > nums[(i+1) % n]:
                 count += 1
                 if count > 1: return False
         return True
+
+print(check([3,4,5,1,2]))
+print(check([2,1,3,4]))
+print(check([1,2,3]))
