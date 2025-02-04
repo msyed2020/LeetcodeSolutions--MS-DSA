76c8fabada5016a2ab2d0a776d62be1f90bddf18
msyed2020 <mikailsyed@yahoo.com>
2025-02-04 17:35:31 -0500

    Examples

--- maxAscendingSum.py
+++ maxAscendingSum.py
@@ -6,3 +6,7 @@ def maxAscendingSum(self, nums: list[int]) -> int:
         curr = curr + nums[i] if nums[i] > nums[i-1] else nums[i]
         res = max(res, curr)
     return res
+
+print(maxAscendingSum([10,20,30,5,10,50]))
+print(maxAscendingSum([10,20,30,40,50]))
+print(maxAscendingSum([12,17,15,13,10,11,12]))
