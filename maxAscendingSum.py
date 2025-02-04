29c6844bd070165f671ed7ac9c45dbecdc9d20d4
msyed2020 <mikailsyed@yahoo.com>
2025-02-04 17:36:57 -0500

    Examples

--- maxAscendingSum.py
+++ maxAscendingSum.py
@@ -1,6 +1,6 @@
 # 1800. Maximum Ascending Subarray Sum
 
-def maxAscendingSum(self, nums: list[int]) -> int:
+def maxAscendingSum(nums: list[int]) -> int:
     curr = res = nums[0]
     for i in range(1, len(nums)):
         curr = curr + nums[i] if nums[i] > nums[i-1] else nums[i]
